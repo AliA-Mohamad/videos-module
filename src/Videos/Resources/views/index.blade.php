6158e75cc895943ffe2564fe9314711d96cf5f99
Ali Mohamad <111379209+AliA-Mohamad@users.noreply.github.com>
2023-03-15 10:31:43 -0300

    Add files via upload

---
+++ src/Videos/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('teste::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('teste.name') !!}
+    </p>
+@endsection
